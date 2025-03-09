2ca1fd5abc1eb7b7252df3e1447f7f9ac57691c9
Erynn Scholtes <foorpyxof@gmail.com>
2025-03-09 21:09:13 +0100

    add more frame types to quic.h

--- fpx_networking/quic/quic.c
+++ fpx_networking/quic/quic.c
@@ -11,3 +11,18 @@ static int _quic_can_send(fpx_quicstream_t* stream) {
   // (a.k.a. is flow control credit reserved?)
   return 0;
 }
+
+static uint8_t _quic_get_varlen(fpx_quic_varlen_t variable) {
+  // to make it a bit easier to handle, we're gonna only grab the
+  // first byte from the variable-length integer, since we only need
+  // the two most significant bits anyway
+
+  uint8_t first_byte = variable.BYTE;
+
+  if (first_byte & 0x80)
+    if (first_byte & 0x40) return 8;  // 0b1100 0000
+    else return 4;                    // 0b1000 0000
+  else
+    if (first_byte & 0x40) return 2;  // 0b0100 0000
+    else return 1;                    // 0b0000 0000
+}
--- fpx_networking/quic/quic.h
+++ fpx_networking/quic/quic.h
@@ -11,6 +11,26 @@
 
 // implements [https://datatracker.ietf.org/doc/html/rfc9000]
 
+typedef union {
+  uint8_t   BYTE;
+  uint16_t  WORD;
+  uint32_t DWORD;
+  uint64_t QWORD;
+} fpx_quic_varlen_t;
+
+struct AckRange {
+  union Value {
+    fpx_quic_varlen_t Gap;
+    fpx_quic_varlen_t AckRangeLength;
+  };
+};
+
+struct EcnCounts {
+  fpx_quic_varlen_t ECT0;
+  fpx_quic_varlen_t ECT1;
+  fpx_quic_varlen_t ECN_CE;
+};
+
 typedef struct {
   uint64_t StreamID;
 
@@ -27,12 +47,73 @@ typedef struct {
   uint64_t StreamID;
   uint16_t Datalen;
   uint8_t Type;
+  // Types:
+  //
+  // 0x00: PADDING      | 0x01: PING          | 0x02: ACK NO ECN  | 0x03: ACK YES ECN
+  //
+  // 0x04: RESET_STREAM | 0x05: STOP_SENDING  | 0x06: CRYPTO      | 0x07: NEW_TOKEN
+  //
+  // 0x08: STREAM       | 0x09: STREAM        | 0x0a: STREAM      | 0x0b: STREAM
+  // 0x0c: STREAM       | 0x0d: STREAM        | 0x0e: STREAM      | 0x0f: STREAM
+  //
+  // 0x10: MAX_DATA     |
+
   union FrameData {
-    struct SingleByte {
-      uint8_t Type; // 0x00 for PADDING, 0x01 for PING
-    };
+
     struct Ack {
-      uint8_t Type;
+      // https://datatracker.ietf.org/doc/html/rfc9000#name-ack-frames
+      fpx_quic_varlen_t LargestAcknowledged; // highest packet number received when acknowledging
+      fpx_quic_varlen_t AckDelay; // (microseconds after receiving frame) / (ack_delay_exponent transport parameter)
+      fpx_quic_varlen_t AckRangeCount;
+      fpx_quic_varlen_t FirstAckOffset; // amount of contiguous packets that came before the LargestAcknowledged
+      struct AckRange* AckRanges;
+      struct EcnCounts* EcnCounts;
+    };
+
+    struct ResetStream {
+      // https://datatracker.ietf.org/doc/html/rfc9000#name-reset_stream-frames
+      fpx_quic_varlen_t ApplicationError;
+      fpx_quic_varlen_t FinalStreamSize;
+    };
+
+    struct StopSending {
+      // https://datatracker.ietf.org/doc/html/rfc9000#name-stop_sending-frames
+      fpx_quic_varlen_t ApplicationError;
+    };
+
+    struct Crypto {
+      // https://datatracker.ietf.org/doc/html/rfc9000#name-crypto-frames
+      fpx_quic_varlen_t Offset; // offset of CryptoData within the whole stream
+      fpx_quic_varlen_t CryptoLength;
+      uint8_t* CryptoData;
+    };
+
+    struct NewToken {
+      // https://datatracker.ietf.org/doc/html/rfc9000#name-new_token-frames
+      // new token for the receiver to use when initializing a new connection to the peer
+      fpx_quic_varlen_t TokenLength;
+      uint8_t* TokenBlob;
+    };
+
+    struct Stream {
+      // https://datatracker.ietf.org/doc/html/rfc9000#name-stream-frames
+      // if fin bit set (Type & 0x01), this is the last frame of the stream
+      // a.k.a.: 
+      fpx_quic_varlen_t Offset; // only if offset bit set (Type & 0x04);
+      // ^^^ this bit is typically set, unless it's either
+      // the first frame of the stream, or the last one that contains no data
+      fpx_quic_varlen_t Length; // only if fin bit set (Type & 0b02)
+      uint8_t* Data;
+    };
+
+    struct MaxData {
+      // https://datatracker.ietf.org/doc/html/rfc9000#name-max_data-frames
+      // sent by the server to indicate flow control credit
+      fpx_quic_varlen_t MaximumData;
+    };
+
+    struct MaxStreamData {
+
     };
     // TODO: add more frame types
   };
